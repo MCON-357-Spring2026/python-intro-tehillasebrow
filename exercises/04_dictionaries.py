ce21ddc3cb465680f1f3ade124eed7f9ada0c766
tehillasebrow <tsebrow@student.touro.edu>
2026-02-01 16:18:15 -0500

    made the book dictionary

--- exercises/04_dictionaries.py
+++ exercises/04_dictionaries.py
@@ -2,3 +2,9 @@
 TODO:
 Create dictionary for a book
 """
+book={
+    "name": "The False Prince",
+    "author": "Jennifer A. Neilsen",
+    "plot": "A prince disguised as a pauper competes with other boys for his own crown",
+    "ending":"He gets the crown :)"
+}
